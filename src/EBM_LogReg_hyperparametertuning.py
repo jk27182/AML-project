aeb6072a7aacf1817524aa9501d8b0f4d53de111
Janik Königshofer <janik@koenigshofer-hh.de>
2023-09-07 11:37:58 +0200

    hyperparameter tuning script for EBM and LogReg

--- src/EBM_LogReg_hyperparametertuning.py
+++ src/EBM_LogReg_hyperparametertuning.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from IPython.display import display
 from functools import partial
@@ -28,7 +29,7 @@ from interpret.glassbox import ExplainableBoostingClassifier, LogisticRegression
 interpret.set_visualize_provider(interpret.provider.InlineProvider())
 
 
-with open('all_data.pickle', 'rb') as file:
+with open('data/all_data.pickle', 'rb') as file:
     all_data = pickle.load(file)
 
 orig_characteristics = all_data['OrigCharacteristics.dta']
@@ -46,7 +47,7 @@ orig_characteristics_columns = [
     'quarter_type',
     'AmortType',
     # 'MSA',
-    'qy',
+    # 'qy',
     'Size',
 
     'OVER_w',
@@ -62,7 +63,8 @@ orig_data_with_dummies = pd.get_dummies(
     columns=[
         'AmortType',
         # 'MSA',
-        'type'
+        'type',
+        'year_priced'
     ]
 )
 clean_data = orig_data_with_dummies[
@@ -81,8 +83,7 @@ X = clean_data.drop(columns=target_col)
 
 sample_size = len(y)
 print('sample size ', sample_size)
-
-global_res_models = {}
+# global_res_models = {'EBM': {'score': [], 'models': [], 'fold': []}, 'LogisticRegression': {'score': [], 'models': [], 'fold': []}}
 # objective for optuna hyperparameter search
 def objective(trial, clf_type, X, y):
     res = {
@@ -90,22 +91,37 @@ def objective(trial, clf_type, X, y):
         'models': [],
     }
     if clf_type == 'EBM':
+        greediness =  trial.suggest_float('greediness',low=0.0, high=0.01, step=0.001) # default is 0
+        max_bins = trial.suggest_int('max_bins', low=128, high=512, log=True) # default is 8
+        inner_bags = trial.suggest_int('inner_bags', low=0, high=3, step=1) # default is 0
+        outer_bags = trial.suggest_int('outer_bags', low=4, high=12, step=4) # default is 0
+        max_rounds = trial.suggest_int('max_rounds', low=4500, high=5500, step=500) # default is 5000
+        early_stopping_rounds = trial.suggest_int('early_stopping_rounds', low=40, high=60, step=15) # default is 50
         learning_rate = trial.suggest_float('learning_rate', 0.0009, 0.0015, step=0.0004)
         smoothing_rounds = trial.suggest_int('smoothing_rounds',0, 4, step=2) # default is 0
 
         model_name = "EBM"
         clf = ExplainableBoostingClassifier(
             random_state=42,
+            max_bins=max_bins,
+            max_rounds=max_rounds,
             learning_rate=learning_rate,
             smoothing_rounds=smoothing_rounds,
+            greediness=greediness,
+            inner_bags=inner_bags,
+            outer_bags=outer_bags,
+            early_stopping_rounds=early_stopping_rounds,
         )
     
     if clf_type == 'LogisticRegression':
         penalty = trial.suggest_categorical("penalty", ["elasticnet", "l2", 'l1'])
         C = trial.suggest_float("C", 0.001, 2, step=0.5)
         class_weight = trial.suggest_categorical('class_weight', ['balanced', None])
-        max_iter = trial.suggest_int('max_iter', 500, 1000, step=100)
+        max_iter = trial.suggest_int('max_iter', 500, 10_000, step=1000)
+        l1_ratio = None
         if penalty == 'elasticnet' or penalty == 'l1':
+            if penalty == 'elasticnet':
+                l1_ratio = trial.suggest_float('l1_ratio', 0.1, 0.9, step=0.4)
             solver = 'saga'
         else:
             solver = trial.suggest_categorical('solver', ['newton-cholesky', 'lbfgs', 'newton-cg'])
@@ -117,7 +133,8 @@ def objective(trial, clf_type, X, y):
             C=C,
             class_weight=class_weight,
             solver=solver,
-            max_iter=max_iter
+            max_iter=max_iter,
+            l1_ratio=l1_ratio
         )
 
     # Use stratified K Fold instead of normal k fold to preserve class imbalance
@@ -141,41 +158,46 @@ def objective(trial, clf_type, X, y):
         print(f'Finished fitting data, took {total_time} sec.')
 
         auc_roc = roc_auc_score(y_test, clf.predict_proba(X_test)[:, 1])
-        res['scores'].append(auc_roc)
-        res['models'].append(clf)
+        # res[model_name]['scores'].append(auc_roc)
+        # res[model_name]['models'].append(clf)
+        # res[model_name]['fold'].append(fold)
 
     trial.set_user_attr(key='scores_and_models', value=res)
-    global_res_models.update({model_name: res})
+    # global_res_models.update({model_name: res})
     return np.mean(auc_roc)
 
 # current best 0.54
 # study_ebm = optuna.create_study(storage="mysql://root@localhost/ebm",direction='maximize', study_name='EBM_study')
 # optuna.delete_study(storage="mysql://root@localhost/ebm", study_name='EBM_study')
-study_ebm = optuna.create_study(
-    direction='maximize',
-    # storage="mysql://root@localhost/ebm",
-    study_name='EBM_study'
-)
-print('test succesful')
-objective_edm = partial(objective, clf_type='EBM', X=X.head(50), y=y.head(50))
-study_ebm.optimize(objective_edm, n_trials=2, show_progress_bar=True)
+n_trials = 25
 
-best_params_ebm = study_ebm.best_params
-best_ebm_value = study_ebm.best_value
-with open('model/best_params_ebm.joblib', 'wb') as file:
-    joblib.dump({'params':best_params_ebm, 'value':best_ebm_value}, file)
+if sys.argv[1] == 'ebm':
+    study_ebm = optuna.create_study(
+        direction='maximize',
+        # storage="mysql://root@localhost/ebm",
+        study_name='EBM_study'
+    )
+    print('test succesful')
+    objective_edm = partial(objective, clf_type='EBM', X=X.head(50), y=y.head(50))
+    study_ebm.optimize(objective_edm, n_trials=n_trials, show_progress_bar=True)
+
+    best_params_ebm = study_ebm.best_params
+    best_ebm_value = study_ebm.best_value
+    with open('models/best_params_ebm.joblib', 'wb') as file:
+        joblib.dump({'params':best_params_ebm, 'value':best_ebm_value}, file)
 
 # no current best
 # optuna.delete_study(storage="mysql://root@localhost/logreg", study_name='LogisticRegression_study')
-study_LR = optuna.create_study(
-    direction='maximize',
-    # storage="mysql://root@localhost/logreg",
-    study_name='LogisticRegression_study'
-)
-objective_LR = partial(objective, clf_type='LogisticRegression', X=X.head(50), y=y.head(50))
-study_LR.optimize(objective_LR, n_trials=50, show_progress_bar=True)
+if sys.argv[1] == 'LR':
+    study_LR = optuna.create_study(
+        direction='maximize',
+        # storage="mysql://root@localhost/logreg",
+        study_name='LogisticRegression_study'
+    )
+    objective_LR = partial(objective, clf_type='LogisticRegression', X=X.head(50), y=y.head(50))
+    study_LR.optimize(objective_LR, n_trials=n_trials, show_progress_bar=True)
 
-best_params_LR = study_LR.best_params
-best_LR_value = study_LR.best_value
-with open('model/best_params_LR.joblib', 'wb') as file:
-    joblib.dump({'params':best_params_LR, 'value':best_LR_value}, file)
+    best_params_LR = study_LR.best_params
+    best_LR_value = study_LR.best_value
+    with open('models/best_params_LR.joblib', 'wb') as file:
+        joblib.dump({'params':best_params_LR, 'auroc_score':best_LR_value}, file)
